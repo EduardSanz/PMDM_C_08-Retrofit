65e24454206462075487fd8d5a0cc4f027bb6ec0
eduard <eduard@cieep.com>
2022-12-12 17:56:40 +0100

    TERMINADO GETS

--- app/src/main/java/com/cieep/a08_retrofit/MainActivity.java
+++ app/src/main/java/com/cieep/a08_retrofit/MainActivity.java
@@ -1,11 +1,19 @@
 package com.cieep.a08_retrofit;
 
+import android.hardware.camera2.TotalCaptureResult;
 import android.os.Bundle;
 
+import com.cieep.a08_retrofit.adapters.AlbumsAdapter;
+import com.cieep.a08_retrofit.conexiones.ApiConexiones;
+import com.cieep.a08_retrofit.conexiones.RetrofitObject;
+import com.cieep.a08_retrofit.modelos.Album;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 
 
@@ -14,26 +22,75 @@ import com.cieep.a08_retrofit.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private ActivityMainBinding binding;
 
+    private List<Album> albums;
+    private AlbumsAdapter adapter;
+    private RecyclerView.LayoutManager lm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setSupportActionBar(binding.toolbar);
+
+        albums = new ArrayList<>();
+        adapter = new AlbumsAdapter(albums, R.layout.album_view_holder, this);
+        lm = new LinearLayoutManager(this);
+
+        binding.contentMain.contenedorAlbums.setAdapter(adapter);
+        binding.contentMain.contenedorAlbums.setLayoutManager(lm);
+
+        doGetAlbums();
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+            }
+        });
+    }
+
+    private void doGetAlbums() {
+        Retrofit retrofit = RetrofitObject.getConexion();
+        ApiConexiones api = retrofit.create(ApiConexiones.class);
+
+        Call<ArrayList<Album>> getAlbums =  api.getAlbums();
+
+        getAlbums.enqueue(new Callback<ArrayList<Album>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Album>> call, Response<ArrayList<Album>> response) {
+                if (response.code() == HttpsURLConnection.HTTP_OK){
+                    ArrayList<Album> temp = response.body();
+                    albums.addAll(temp);
+                    adapter.notifyItemRangeInserted(0, temp.size());
+                }
+                else {
+                    Toast.makeText(MainActivity.this, response.message(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Album>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "NO TIENES INTERNETE", Toast.LENGTH_SHORT).show();
+                Log.e("FAILURE", t.getLocalizedMessage());
             }
         });
     }
--- app/src/main/java/com/cieep/a08_retrofit/conexiones/RetrofitObject.java
+++ app/src/main/java/com/cieep/a08_retrofit/conexiones/RetrofitObject.java
@@ -13,4 +13,5 @@ public class RetrofitObject {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
+
 }
